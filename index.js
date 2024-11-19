1c918fe09f066cdd9180938890ff7d902f62d614
nemecro <nemecstudies@gmail.com>
2024-11-19 15:39:02 +0100

    create get, update and print methods for the gameboard object

---
+++ index.js
@@ -0,0 +1,22 @@
+const gameboard = (function(){
+    board = [];
+    for (let i = 0; i < 9; i++){
+        board.push('');
+    }
+
+    const get = () => board;
+
+    const update = function(index, value){
+        board[index] = value;
+    }
+
+    const print = function(){
+        console.log(board[0], board[1], board[2]);
+        console.log(board[3], board[4], board[5]);
+        console.log(board[6], board[7], board[8]);
+    }
+
+    return {get, update, print}
+})();
+
+gameboard.print();
